15877db682bf6489b831054d24bbe807d7cba3d5
Carlos Martin <s_martincj@globalhitss.com>
2023-04-07 02:42:12 -0600

    logica de componentes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,5 @@
 
 @section('contenido')
 
-    @if ($posts->count())
-    
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @foreach ($posts as $post)
-                <div>
-                    <a href="{{route('posts.show', ['post' => $post, 'user' => $post->user])}}">
-                        <img src="{{asset('uploads/'.$post->imagen)}}" alt="Imagen de post {{$post->titulo}}" />  
-                    </a>
-                </div>
-            @endforeach
-        </div>
-        <div class="my-10">
-            {{$posts->links()}}
-        </div>
-        
-    @else
-        
-    @endif
+    <x-listar-posts :posts="$posts" />
 @endsection
